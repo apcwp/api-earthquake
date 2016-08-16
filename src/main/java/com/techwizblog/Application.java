01d70739b6f9f69fedd860e2ee793e0d7c9f5f5d
SIB <techwizblog>
2016-08-16 11:22:53 +1000

    api-earthquake

--- src/main/java/com/techwizblog/Application.java
+++ src/main/java/com/techwizblog/Application.java
@@ -2,11 +2,13 @@ package com.techwizblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by SIB on 8/12/2016.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
